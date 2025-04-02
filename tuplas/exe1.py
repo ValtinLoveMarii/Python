f9afc66be60888822df37fe493ba86e3ed0f4f4f
Valtin_Loves_Mari!!! <guilherme.toledo.m17@gmail.com>
2025-04-01 21:40:28 -0300

    Commit 8
    Adicionei os arquivos da aula sobre TUPLA

---
+++ tuplas/exe1.py
@@ -0,0 +1,25 @@
+#EXERCÍCIOS DE TUPLA
+
+#EX1 - Crie uma tupla chamada minha_tupla contendo os números 1, 2, 3, 4, 5. Em seguida, imprima o segundo elemento da tupla.
+# minha_tupla = (1,2,3,4,5)
+# print(f" Minha tupla, com o segundo valor: {minha_tupla[1]}")
+
+#EX2 - exe1 mas criado a tupla
+# lista = []
+# for i in range(1, 6):
+#     n = int(input('Digite um número: '))
+#     lista.append(n)
+# lista = tuple(lista)
+# print(f"""Sua tupla foi criada! O valor do segundo número: {lista[1]} O tipo de dado: {type(lista)}
+#     """)
+
+#EX3 - Dada a tupla frutas = ("maçã", "banana", "laranja", "uva"), imprima: O primeiro e o último elemento da tupla. O terceiro elemento usando índice negativo.
+# lista = []
+# for i in range(1, 7):
+#     n = str(input('Digite uma fruta: '))
+#     lista.append(n)
+# lista = tuple(lista)
+# print(lista)
+# print(f"Primeiro índice {lista[0]}")
+# print(f"Ultimo índice {lista[-1]}")
+# print(f"Terceiro ultimo índice {lista[-3]}")
---
+++ tuplas/tuplas.py
@@ -0,0 +1,8 @@
+# Aula 27 - Tuplas
+tupla = (1,2,3,4,5)
+# tupla = list(tupla)
+# print(type(tupla))
+# tupla.append(12)      ------- MUDAR UMA TUPLA, SERIA TRANSFORMAR EM LSITA E ADICIONAR NA LISTA E MUDAR DE NOVO
+# print(tupla)
+# tupla = tuple(tupla)
+# print(type(tupla))
