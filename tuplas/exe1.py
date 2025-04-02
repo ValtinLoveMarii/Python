b717c569f5b7bd8f8387d5a667cea67345dbf4d9
Valtin_Loves_Mari!!! <guilherme.toledo.m17@gmail.com>
2025-04-02 13:22:17 -0300

    Commit 9
    Adicionei os exercícios 4 ao 7 no exe1.py de TUPLA

--- tuplas/exe1.py
+++ tuplas/exe1.py
@@ -23,3 +23,51 @@
 # print(f"Primeiro índice {lista[0]}")
 # print(f"Ultimo índice {lista[-1]}")
 # print(f"Terceiro ultimo índice {lista[-3]}")
+
+#EX4 - Dada a tupla tupla1 = (1, 2, 3) e a tupla tupla2 = (4, 5, 6), crie uma nova tupla tupla3 que seja a concatenação de tupla1 e tupla2. Imprima tupla3.
+# tupla1 = (1,2,3)
+# tupla2 = (4,5,6)
+# tupla3 = tupla1 + tupla2
+# print(f'Nova tupla com a soma das 2 {tupla3}')
+
+#EX5 - Crie uma tupla chamada numeros = (1, 3, 5, 7, 9) e verifique se o número 5 está presente na tupla. Imprima "Sim" se estiver presente, "Não" caso contrário.
+# tupla = (1,3,8,7,9, 5)
+# exist = False
+# for i in tupla:
+#     if i == 5:
+#         print(f'Sim existe o número {i}')
+#         exist = True
+#         break
+#     else:
+#         print('Ainda não presente! ')
+#         continue
+# if(exist == False):
+#     print('Dentro da lista não existe o número 5')
+
+#EX6 - Dada a tupla dados = ('João', 25, 'Mestre'), desempacote ela em três variáveis chamadas nome, idade e cargo. Depois, imprima essas variáveis.
+# tupla = ('João', 25, 'Mestre')
+# l = []
+# for i in range(len(tupla)):
+#     globals ()[f'variavel{i}'] = None
+# print(globals())
+# for x in range(len(tupla)):
+#     globals()[f'variavel{x}'] = tupla[x]
+
+# for z in range(len(tupla)):
+#     l.append(globals()[f'variavel{z}'])
+# print(l)
+
+# z = 2
+# print(globals()[f'variavel{z}'])
+
+#EX7 - Dada a tupla tupla = (1, 2, 3, 4, 2, 5, 2), conte quantas vezes o número 2 aparece na tupla e imprima o resultado.
+# tupla = (1, 2, 3, 4, 2, 5, 2)
+# l = []
+# print(f'O numero 2 aparece {tupla.count(2)} vezes')
+# for i in tupla:
+#     if i == 2:
+#         l.append(i)
+# print(f'O número 2 aparece {len(l)} vezes')
+
+
+
