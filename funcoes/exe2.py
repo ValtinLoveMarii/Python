5945cba8d034f27d71aba446fabcc31c1accfceb
Valtin_Loves_Mari!!! <guilherme.toledo.m17@gmail.com>
2025-05-01 20:02:02 -0300

    Commit 32
    Finalizei o mini projeto de análise de vendas de uma Loja

--- funcoes/exe2.py
+++ funcoes/exe2.py
@@ -17,54 +17,71 @@
 
 # EX2 - PROJETO ANALISE DE DADOS BASICO Consiste em uma analise sobre uma loja
 
-#BASE DE DADOS de vendas da LOJA
+# #BASE DE DADOS de vendas da LOJA
 vendas = [
-    {'dia': 'segunda', 'produto': 'camiseta', 'quantidade': 3, 'preco_unitario': 50},
+    {'dia': 'segunda', 'produto': 'camiseta', 'quantidade': 72, 'preco_unitario': 50},
     {'dia': 'segunda', 'produto': 'calça', 'quantidade': 1, 'preco_unitario': 120},
-    {'dia': 'terça', 'produto': 'camiseta', 'quantidade': 2, 'preco_unitario': 50},
-    {'dia': 'quarta', 'produto': 'tênis', 'quantidade': 1, 'preco_unitario': 200},
-    {'dia': 'quarta', 'produto': 'calça', 'quantidade': 2, 'preco_unitario': 120},
-    {'dia': 'quinta', 'produto': 'camiseta', 'quantidade': 1, 'preco_unitario': 50},
-    {'dia': 'sexta', 'produto': 'tênis', 'quantidade': 1, 'preco_unitario': 200},
+    {'dia': 'terça', 'produto': 'camiseta', 'quantidade': 5, 'preco_unitario': 50},
+    {'dia': 'quarta', 'produto': 'tênis', 'quantidade': 12, 'preco_unitario': 200},
+    {'dia': 'quarta', 'produto': 'calça', 'quantidade': 9, 'preco_unitario': 120},
+    {'dia': 'quinta', 'produto': 'camiseta', 'quantidade': 11, 'preco_unitario': 250},
+    {'dia': 'sexta', 'produto': 'tênis', 'quantidade': 71, 'preco_unitario': 200},
 ]
 
 #DADOS A SEREM EXTRAIDOS
+def prods_sales(vendas):
+    tot_prods_sales = {}
+    for v in vendas:
+        prod = v['produto']
+        qtd = v['quantidade']
+        if prod in tot_prods_sales:
+            tot_prods_sales[prod] += qtd
+        else:
+            tot_prods_sales[prod] = qtd
+    return tot_prods_sales
+result_total_prods = prods_sales(vendas)
+
+#PRODUTO COM A MAIOR QUANTIDADE DE VENDAS
+def more_sales(result_total_prods):
+    maior = 0
+    name_prod = None
+    for i,z in result_total_prods.items():
+        if z > maior:
+            maior = z
+            name_prod = i
+    return name_prod
+res_more_sales = more_sales(result_total_prods)
 
 
-#Total de vendas por produtos
 
-
-
-#CRIA A LISTA COM TODOS OS PRODUTOS
-prods_values_list = []
-def get_prods(prods_values):
-    for i in vendas:
-        values = i["produto"]
-        prods_values_list.append(values)
-    return prods_values_list
-
-prods_values = get_prods(prods_values_list)
-
-
-#CRIA O DICIONARIO COM OS TOTAL DE VENDAS POR PRODUTO
-
-def tot_sales(prods_values):
-    dic_res = {}
-    x_index = 0
-    for i in vendas:
-        while x_index < len(prods_values):
-            x = prods_values[x_index]
-            if x not in dic_res:
-                qtd = i['quantidade']
-                dic_res.update({x:qtd})
-                x_index += 1  
-                break
-            else:
-                qtd =i['quantidade']
-                dic_res[x] += qtd
-                x_index += 1  
-    return dic_res
-result_total_sales = tot_sales(prods_values)
-print(f'Total de Vendas da Loja!')
-for x,z in result_total_sales.items():
-    print(x,z)
+#DIA COM MAIOR FATURAMENTO
+def day_more_sales_list(vendas):
+    day_sales = {}
+    for d in vendas:
+        day = d['dia']
+        price = d['preco_unitario']
+        if day in day_sales:
+            day_sales[day] += price
+        else:
+            day_sales[day] = price
+    maior = 0
+    name_day = None
+    for i,z in day_sales.items():
+        if z > maior:
+            maior = z
+            name_day = i
+    return day_sales, name_day
+day_sales,name_day = day_more_sales_list(vendas)
+print('Relatório de Vendas')
+print('--'*20)
+print('Quantidade de Vendas por produto')
+for i,z in result_total_prods.items():
+    print(f'{i}: {z}')
+print('--'*20)
+print('Faturamento diário')
+for x,m in day_sales.items():
+    print(f'{x}: {m}')
+print('--'*20)
+print(f'Dia com o maior faturamento: {name_day}')
+print(f'Produto com maior número de vendas: {res_more_sales}')
+print('--'*20)
