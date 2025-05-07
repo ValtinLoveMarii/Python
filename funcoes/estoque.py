0abcf8a91ca3ed1d0a9bcac37fbe3a58eb194e38
Valtin_Loves_Mari!!! <guilherme.toledo.m17@gmail.com>
2025-05-07 20:26:04 -0300

    Commit 34
    Atualizei por completo o exercício estoque.py em FUNÇÃO

--- funcoes/estoque.py
+++ funcoes/estoque.py
@@ -1,58 +1,36 @@
 import os
-items_produtos = {}
-numero_prod = 0
+
+produtos_dicionario = {}
 for i in range(1):
-    nome_prod = str(input('Qual produto a ser cadastrado? '))
-    quantidade = int(input('Qual a quantidade do produto? '))
-    preco = float(input('Qual o valor por unidade? '))
-    numero_prod = str(numero_prod)
-    items_produtos['Produto_'+numero_prod] = {'nome':nome_prod, 'quantidade':quantidade, 'preco':preco}
-    numero_prod = int(numero_prod)
-    numero_prod += numero_prod
+    nome_prod = str(input('Digite o produto: ')).lower()
+    quantidade = int(input('Digite a quantidade de unidades: '))
+    preco = float(input('Digite o preço da unidade: '))
+    produtos_dicionario[nome_prod] = [quantidade, preco]
     os.system('cls')
 
-
-#ADICONAR PRODUTO
-def adiconar_produtos(items_produtos):
-    numero_prod = 1
-    nome_prod = str(input('Qual produto a ser cadastrado? '))
-    quantidade = int(input('Qual a quantidade do produto? '))
-    preco = float(input('Qual o valor por unidade? '))
-    numero_prod = str(numero_prod)
-    items_produtos.update({'Produto_'+numero_prod:{'nome':nome_prod,'quantidade':quantidade, 'preco':preco}})
-    numero_prod = int(numero_prod)
-    numero_prod += numero_prod
+def adicionar_produtos(produtos_dicionario):
+    nome_prod = str(input('Digite o novo valor: ')).lower()
+    quantidade = int(input('Digite a quantidade de unidades: '))
+    preco = float(input('Digite o preço da unidade: '))
+    produtos_dicionario[nome_prod] = [quantidade, preco]
     os.system('cls')
-adiconar_produtos(items_produtos)
+adicionar_produtos(produtos_dicionario)
 
-#REMOVER PRODUTO
-def remover_produto(items_produtos):
-    remover = str(input('Qual produto deseja remover? ')).lower()
-    if remover == 'n' or remover == 'nao':
-        pass
+def remover_produto(produtos_dicionario):
+    remove_prod = str(input('Qual produto deseja retirar: '))
+    if remove_prod in produtos_dicionario:
+        produtos_dicionario.pop(remove_prod)
     else:
-        items_produtos.pop('Produto_'+remover)
-    os.system('cls')
-remover_produto(items_produtos)
+        print('Esse produto não existe!')
+# remover_produto(produtos_dicionario)
 
-
-#ATUALIZAR QUANTIDADE
-def atualizar_quantidade(items_produtos):
-    prod_atualizar = str(input('Qual produto atualizar? '))
-    prod_atualizar = 'Produto_'+prod_atualizar
-    if prod_atualizar in items_produtos:
-        atu = int(input(f'Qual a nova quantidade do {prod_atualizar}? '))
-        var = items_produtos[prod_atualizar]
-        var['quantidade'] = atu
-    else:
-        print('Produto já removido!')
-atualizar_quantidade(items_produtos)
-
-
-#MOSTRAR O ESTOQUE
-def listar_estoque(items_produtos):
-    for i,z in items_produtos.items():
-        print(i, z)
-listar_estoque(items_produtos)
-
-#SISTEMA INTEGRADO COMPLETO
+def atualizar_produtos(produtos_dicionario):
+    resp_atualizar = str(input('Qual produto deseja atualizar: ')).lower()
+    if resp_atualizar not in produtos_dicionario:
+        print('Esse produto não existe!')
+    else:    
+        new_quantidade = int(input(f'Qual a nova quantidade do produto {resp_atualizar}: '))
+        new_preco = float(input(f'Qual o novo preço do produto {resp_atualizar}: '))
+        produtos_dicionario[resp_atualizar] = [new_quantidade, new_preco]
+atualizar_produtos(produtos_dicionario)
+print(produtos_dicionario)
