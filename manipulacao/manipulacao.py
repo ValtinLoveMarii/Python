6898229d38eaf081ff45499b688169da1cb19a62
Valtin_Loves_Mari!!! <guilherme.toledo.m17@gmail.com>
2025-05-20 17:20:24 -0300

    commit 40
    Adicionei os arquivos e pasta de manipulacao

---
+++ manipulacao/manipulacao.py
@@ -0,0 +1,14 @@
+#AULA 32 - MANIPULAÇÃO de ARQUIVOS
+import os
+# print(os.name)
+
+print(os.path.exists('meuspacs.txt'))
+print(os.path.exists('if/if_else_elif.py'))
+# os.mkdir('pasta')
+# os.makedirs('manipulacao', exist_ok=True)
+# with open('manipulacao/arquuivo.py', 'w', encoding='utf-8') as f:
+#     f.write('Esse é o novo arquivo legal')
+# os.remove('manipulacao/arquuivo.py')
+# os.removedirs('pasta')
+
+# os.rename('./manipulacao/arquuivo.py', './manipulacao/arquivinho.py')
