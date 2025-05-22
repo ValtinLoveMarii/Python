97142f47638b5aedddf6c09d42f9d0a6fe392f6c
Valtin_Loves_Mari!!! <guilherme.toledo.m17@gmail.com>
2025-05-22 11:43:07 -0300

    commit 44
    Atualizei o estoque.py

--- funcoes/estoque.py
+++ funcoes/estoque.py
@@ -1,69 +1,92 @@
 import os
 
 produtos_dicionario = {}
-for i in range(1):
-    nome_prod = str(input('Digite o produto: ')).lower()
-    quantidade = int(input('Digite a quantidade de unidades: '))
-    preco = float(input('Digite o preço da unidade: '))
-    produtos_dicionario[nome_prod] = [quantidade, preco]
-    os.system('cls')
-#--------------------------------------------------------------------
+
+
+def limpar_tela():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def cabecalho(msg):
+    print("\n" + "=" * 40)
+    print(f"{msg.center(40)}")
+    print("=" * 40)
+
+
 def adicionar_produtos(produtos_dicionario):
-    nome_prod = str(input('Digite o novo valor: ')).lower()
+    limpar_tela()
+    cabecalho("Adicionar Produto")
+    nome_prod = str(input('Digite o nome do produto: ')).lower()
     quantidade = int(input('Digite a quantidade de unidades: '))
     preco = float(input('Digite o preço da unidade: '))
     produtos_dicionario[nome_prod] = [quantidade, preco]
-    os.system('cls')
-# adicionar_produtos(produtos_dicionario)
-#--------------------------------------------------------------------
+    print("\nProduto adicionado com sucesso!")
+    input("\nPressione Enter para continuar...")
+
+
 def remover_produto(produtos_dicionario):
-    remove_prod = str(input('Qual produto deseja retirar: '))
+    limpar_tela()
+    cabecalho("Remover Produto")
+    remove_prod = str(input('Qual produto deseja remover: ')).lower()
     if remove_prod in produtos_dicionario:
         produtos_dicionario.pop(remove_prod)
+        print(f"\nProduto '{remove_prod}' removido com sucesso!")
     else:
-        print('Esse produto não existe!')
-# remover_produto(produtos_dicionario)
+        print('\nEsse produto não existe!')
+    input("\nPressione Enter para continuar...")
 
-#--------------------------------------------------------------------
+
 def atualizar_produtos(produtos_dicionario):
+    limpar_tela()
+    cabecalho("Atualizar Produto")
     resp_atualizar = str(input('Qual produto deseja atualizar: ')).lower()
     if resp_atualizar not in produtos_dicionario:
         print('Esse produto não existe!')
-        os.system('cls')
-    else:    
+    else:
         new_quantidade = int(input(f'Qual a nova quantidade do produto {resp_atualizar}: '))
         new_preco = float(input(f'Qual o novo preço do produto {resp_atualizar}: '))
         produtos_dicionario[resp_atualizar] = [new_quantidade, new_preco]
-#--------------------------------------------------------------------
+        print(f"\nProduto '{resp_atualizar}' atualizado com sucesso!")
+    input("\nPressione Enter para continuar...")
+
+
 def listar_produto(produtos_dicionario):
-    for i,z in produtos_dicionario.items():
-        print(f'{i} : {z}')
+    limpar_tela()
+    cabecalho("Lista de Produtos")
+    if not produtos_dicionario:
+        print("\nNenhum produto cadastrado no estoque.")
+    else:
+        for i, z in produtos_dicionario.items():
+            print(f"{i.title()} - Quantidade: {z[0]} - Preço: R${z[1]:.2f}")
+    input("\nPressione Enter para continuar...")
 
 
-
-#--------------------------------------------------------------------
 # SISTEMA FINAL
 while True:
-    print('SISTEMA DE ESTOQUE')
-    resp = input(('ADICIONAR PRODUTO[1]\nREMOVER PRODUTO[2]\nATUALIZAR PRODUTO[3]\nLISTAR PRODUTO[4]\nSAIR[5]\n\nDesejo: '))
+    limpar_tela()
+    cabecalho("Sistema de Estoque")
+    print("""
+    [1] - Adicionar Produto
+    [2] - Remover Produto
+    [3] - Atualizar Produto
+    [4] - Listar Produtos
+    [5] - Sair
+    """)
+
+    resp = input('Escolha uma opção: ')
     if resp == '1':
-        os.system('cls')
         adicionar_produtos(produtos_dicionario)
     elif resp == '2':
-        os.system('cls')
         remover_produto(produtos_dicionario)
     elif resp == '3':
-        os.system('cls')
         atualizar_produtos(produtos_dicionario)
     elif resp == '4':
-        os.system('cls')
         listar_produto(produtos_dicionario)
     elif resp == '5':
+        limpar_tela()
+        print('FIM DO ESTOQUE'.center(40, "="))
         break
     else:
-        os.system('cls')
-        print('Dado Inválido!')
-        print('Tente Novamente!')
-os.system('cls')
-print('FIM DO ESTOQUE')
-
+        limpar_tela()
+        print("Opção inválida! Tente novamente.".center(40, "="))
+        input("\nPressione Enter para continuar...")
