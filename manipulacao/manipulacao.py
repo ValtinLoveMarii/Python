e32733a4b61b46857febd036cb7fa864999e457d
Valtin_Loves_Mari!!! <guilherme.toledo.m17@gmail.com>
2025-05-26 12:39:48 -0300

    Commit 45
    Atualizei o manipulacao.py

--- manipulacao/manipulacao.py
+++ manipulacao/manipulacao.py
@@ -12,3 +12,24 @@ print(os.path.exists('if/if_else_elif.py'))
 # os.removedirs('pasta')
 
 # os.rename('./manipulacao/arquuivo.py', './manipulacao/arquivinho.py')
+
+dic ={}
+with open('dados.txt','r') as f:
+    arquivo = f.readlines()
+for x in arquivo:
+    cols = x.strip().split()
+    dic.update({cols[0]:cols[1]})
+print(dic)
+
+# with open('dados.txt', 'a') as f:
+#     f.write('\nRAIT DE RUMBA VAI DE RAIT DE RUMBA')
+
+while True:
+    aaa = str(input('Criar arquivo: '))
+    with open(f'{aaa}.txt', 'w') as f:
+        f.write('Criado com Sucesso')
+    resp = str(input('Continuar? '))
+    if resp == 's':
+        continue
+    else:
+        break
