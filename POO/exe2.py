33a600d14ab7a3b44e18f52c409544651a017958
Valtin_Loves_Mari!!! <guilherme.toledo.m17@gmail.com>
2025-07-02 12:07:31 -0300

    Commit 78

--- POO/exe2.py
+++ POO/exe2.py
@@ -75,6 +75,167 @@ class Votacao():
                 self.listaVotos[valor] += 1
     
     def resultado(self):
+
+#EX3 - Crie uma classe base chamada Veiculo com os atributos: marca modelo ano Adicione também um método mostrar_info que imprima os dados do veículo. Depois, crie uma subclasse chamada Carro que herde de Veiculo e adicione um atributo: numero_de_portas Implemente o método mostrar_info na subclasse para incluir também o número de portas do carro.
+class Veiculo():
+    def __init__(self, marca, modelo, ano):
+        self._marca = marca
+        self._modelo = modelo
+        self._ano = ano
+        
+    def mostrar_info(self, marca, modelo, ano, portas):
+        print(f'Marca:{marca}, Modelo:{modelo}, Ano:{ano}, Portas:{portas}')
+        
+class Carro(Veiculo):
+    def __init__(self, portas,marca, modelo, ano):
+        super().__init__(marca, modelo, ano)
+        self._portas = portas
+        
+    def mostrar_info(self):
+        super().mostrar_info(self._marca, self._modelo, self._ano, self._portas)
+        
+# c1 = Carro(4, 'Mercedes', 'Benz', 2025)
+# c1.mostrar_info()
+
+#EX4 - Crie uma classe base chamada Funcionario com os seguintes atributos privados nome  salario Crie getters e setters para esses atributos. Em seguida, crie uma subclasse chamada Gerente, que herda de Funcionario e adicione um atributo privado: bonus Adicione um getter e setter para o atributo bonus na subclasse.
+class Funcionario():
+    def __init__(self, nome, salario):
+        self._nome = nome
+        self._salario = salario
+    
+    """GET e SET"""
+    @property
+    def nome(self):
+        return self._nome
+    
+    @property
+    def salario(self):
+        return self._salario
+    
+class Gerente(Funcionario):
+    def __init__(self, nome, salario, bonus):
+        super().__init__(nome, salario)
+        self._bonus = bonus
+    
+    """GET e SET"""
+    @property
+    def bonus(self):
+        return self._bonus
+    
+    def salario_gerente(self):
+        print(f'Salário gerente: {self.salario + self.bonus}')
+        
+# g1 = Gerente('Edvander', 12000, 500)
+# g1.salario_gerente()
+
+#EX4 - Crie uma classe base chamada Produto com os atributos: nome preco Em seguida, crie uma subclasse chamada ProdutoImportado que herde de Produto e adicione o atributo: taxa_imposto Na subclasse, crie um método preco_final que calcule o preço final do produto considerando o imposto (taxa de imposto sobre o preço).
+class Produto():
+    def __init__(self, nome, preco):
+        self._nome = nome
+        self._preco = preco
+
+    @property
+    def nome(self):
+        return self._nome
+    
+    @property
+    def preco(self):
+        return self._preco
+class Importado(Produto):
+    def __init__(self, nome, preco):
+        super().__init__(nome, preco)
+        self._taxa = 2
+    @property
+    def taxa(self):
+        return self._taxa
+    
+    def preco_final(self):
+        print(f'Preço final: {self.preco + self.taxa}')
+
+# p = Importado('Leite', 9)
+# p.preco_final()
+
+#EX5 - Crie uma classe base chamada Pessoa com os atributos: nome cpf (esse atributo deve ser privado) Na subclasse Aluno, crie um método mostrar_dados para exibir o nome e o CPF do aluno. Contudo, use o getter para acessar o cpf, e o setter para alterá-lo.
+class Pessoa():
+    def __init__(self, nome, cpf):
+        self._nome = nome
+        self.__cpf = cpf
+    
+    """GET e SET"""
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @property
+    def cpf(self):
+        return self.__cpf
+
+class Aluno(Pessoa):
+    def __init__(self, nome, cpf):
+        super().__init__(nome, cpf)
+    
+    def mostrar_dados(self):
+        print(self.cpf)
+        
+# a1 = Aluno('Alex', 999001)
+# a1.mostrar_dados()
+
+#EX6 - BANCO DO MINE
+class Base():
+    def __init__(self, nome, idade, cpf, sexo):
+        self.__nome = nome
+        self.__idade = idade
+        self.__cpf = cpf
+        self.__sexo = sexo
+    
+    
+    """GET"""
+    @property
+    def nome(self):
+        return self.__nome
+
+    @property
+    def idade(self):
+        return self.__idade
+    
+    @property
+    def cpf(self):
+        return self.__cpf
+    
+    @property
+    def sexo(self):
+        return self.__sexo
+    
+class ClientePF(Base):
+    def __init__(self, nome, idade, cpf, sexo, profissao):
+        super().__init__(nome, idade, cpf, sexo)
+        self.__profissao = profissao
+    
+    """GET"""
+    @property
+    def profissao(self):
+        return self.__profissao
+
+class ClientePJ(Base):
+    def __init__(self, nome, idade, cpf, sexo, razao_social):
+        super().__init__(nome, idade, cpf, sexo)
+        self.__razao_social = razao_social
+
+    """GET"""
+    
+    @property
+    def razao_social(self):
+        return self.__razao_social
+
+class OrgaoPublico(Base):
+    def __init__(self, nome, idade, cpf, sexo, esfera_governo):
+        super().__init__(nome, idade, cpf, sexo)
+        self.__esfera_governo = esfera_governo
+    
+    """GET"""
+    def esfera_governo(self):
+        return self.__esfera_governo
+    
         self.votar()
         print(self.listaVotos)
         if self.listaVotos['A'] > self.listaVotos['B']:
